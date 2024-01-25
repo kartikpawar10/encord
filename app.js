9552e07874e0c73043816144cdd7cef31cb142a1
kartikpawar10 <kartikpawarofficialon@gmail.com>
2024-01-25 23:15:48 +0530

    Juned's Login

--- app.js
+++ app.js
@@ -3,9 +3,6 @@ const { urlencoded } = require("body-parser");
 const express = require("express")
 const app = express();
 const connectDB = require("./db/connect")
-app.use(express.static('./public'))
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 const start = async()=>{
     try {
         await connectDB();
@@ -15,4 +12,9 @@ const start = async()=>{
     }
 }
 
+app.use(express.static('./public'))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('api/v1/task',)
+app.listen(5000)
 start()
--- controller/main.js
+++ controller/main.js
@@ -5,11 +5,11 @@ const login = async (req, res) => {
   if (!username || !password) {
     return res.json({ msg: "Please provide correct credentials" });
   }
-
   const id = new Date().getDate();
   const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
     expiresIn: "30d",
   });
+  res.status(200).json({ msg: 'user created', token })
 };
 
 const register = async (req, res) => {};
